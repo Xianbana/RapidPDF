ec782e5760843ff869f7781f66794ff24482f948
xianban <1196525229@qq.com>
2024-07-22 10:01:42 +0800

    Initial Commit

---
+++ RapidPDF/RapidPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapidPDFApp.swift
+//  RapidPDF
+//
+//  Created by 周波 on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RapidPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

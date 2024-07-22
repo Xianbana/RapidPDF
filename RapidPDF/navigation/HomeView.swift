e02938db6d66a41843f7d06910dfc76fac2ac805
xianban <1196525229@qq.com>
2024-07-22 10:28:36 +0800

    pdf project init

---
+++ RapidPDF/navigation/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  RapidPDF
+//
+//  Created by 周波 on 2024/7/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

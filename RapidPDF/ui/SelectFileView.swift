0054d3ea0949c174a6322926d139189ec4f587cc
xianban <1196525229@qq.com>
2024-07-22 18:47:59 +0800

    no mm swift ui

---
+++ RapidPDF/ui/SelectFileView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectFileView.swift
+//  RapidPDF
+//
+//  Created by 周波 on 2024/7/22.
+//
+
+import SwiftUI
+
+struct SelectFileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectFileView()
+}
---
+++ RapidPDF/util/DataUtil.swift
@@ -0,0 +1,8 @@
+//
+//  DataUtil.swift
+//  RapidPDF
+//
+//  Created by 周波 on 2024/7/22.
+//
+
+import Foundation
